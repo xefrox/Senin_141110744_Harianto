b041b13b05997c0b13ac530327fa4405be8df399
Harianto <afie_zhang@yahoo.com>
2016-10-10 19:38:24 +0700

    Latihan_2_1

---
+++ Latihan_2_1/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Latihan_2_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.domainUpDown1 = new System.Windows.Forms.DomainUpDown();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.CalendarDimensions = new System.Drawing.Size(2, 2);
+            this.monthCalendar1.FirstDayOfWeek = System.Windows.Forms.Day.Sunday;
+            this.monthCalendar1.Location = new System.Drawing.Point(61, 48);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(363, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(444, 16);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Remove";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // domainUpDown1
+            // 
+            this.domainUpDown1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.domainUpDown1.Items.Add("January");
+            this.domainUpDown1.Items.Add("February");
+            this.domainUpDown1.Items.Add("March");
+            this.domainUpDown1.Items.Add("April");
+            this.domainUpDown1.Items.Add("May");
+            this.domainUpDown1.Items.Add("June");
+            this.domainUpDown1.Items.Add("July");
+            this.domainUpDown1.Items.Add("August");
+            this.domainUpDown1.Items.Add("September");
+            this.domainUpDown1.Items.Add("October");
+            this.domainUpDown1.Items.Add("November");
+            this.domainUpDown1.Items.Add("December");
+            this.domainUpDown1.Location = new System.Drawing.Point(204, 16);
+            this.domainUpDown1.Name = "domainUpDown1";
+            this.domainUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.domainUpDown1.TabIndex = 3;
+            this.domainUpDown1.Text = "Month";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(61, 16);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            31,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 361);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(596, 401);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.domainUpDown1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.monthCalendar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DomainUpDown domainUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Latihan_2_1/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Latihan_2_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            DateTime sm = new DateTime(2016, 1, 1);
+            for (int i = 0; i < 365; i++)
+            {
+                if (sm.DayOfWeek == DayOfWeek.Saturday || sm.DayOfWeek == DayOfWeek.Sunday)
+                {
+                    monthCalendar1.AddBoldedDate(sm);
+                }
+                sm = sm.AddDays(1);
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            domainUpDown1.Text = "Month";
+            DateTime lahir = new DateTime(1996,7,12);
+            monthCalendar1.AddAnnuallyBoldedDate(lahir);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        { 
+            
+            DateTime ne = new DateTime();
+            decimal a;
+            int b;
+            b = domainUpDown1.SelectedIndex + 1;
+            a = numericUpDown1.Value;
+            ne = new DateTime(2016, b, (int)a);
+            monthCalendar1.AddAnnuallyBoldedDate(ne);
+            monthCalendar1.UpdateBoldedDates();
+            monthCalendar1.SetDate(ne);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DateTime ne = new DateTime();
+            decimal a;
+            int b;
+            b = domainUpDown1.SelectedIndex + 1;
+            a = numericUpDown1.Value;
+            ne = new DateTime(2016, b, (int)a);
+            monthCalendar1.RemoveAnnuallyBoldedDate(ne);
+            monthCalendar1.UpdateBoldedDates();
+            monthCalendar1.SetDate(ne);
+        }
+    }
+}
